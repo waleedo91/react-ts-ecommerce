725739825fd16d93455b3753cf1f9cae0e4587d8
Waleed <wsaleh1991@gmail.com>
2025-07-02 11:42:57 -0500

    Still having issues with firebase in workflow tests making another attempt after updating config file

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "mock-api-key",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "mock-auth-domain",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "mock-project-id",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "mock-storage",
+  messagingSenderId:
+    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "mock-msg-id",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "mock-app-id",
+  measurementId:
+    import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "mock-measurement-id",
 };
 
 const app = initializeApp(firebaseConfig);
